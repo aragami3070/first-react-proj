f816d76e7e0bcddd2aea2d1dabed2a1d41f0ab71
aragami3070 <smirnov17612@gmail.com>
2026-03-28 13:49:02 +0400

    Chore(home-page): change duplicate components to GridBackGroundLayout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,106 +1,42 @@
-import { Box, Button, Container, Typography, useTheme } from "@mui/material";
-import { alpha } from "@mui/material/styles";
+import { Button, Typography, useTheme } from "@mui/material";
 import { Link } from "react-router-dom";
+import { CenterFullScreenLayout } from "../components/CenterFullScreenLayout";
+import { GridBackGroundLayout } from "../components/GridBackGroundLayout";
 
 export default function Home() {
 
   const theme = useTheme();
   return (
     <>
-      <Box
-        sx={(theme) => {
-          const gridColor = alpha(theme.palette.divider, 0.19);
-          return {
-            minHeight: '100vh',
-            minWidth: '100vw',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            // сетка поверх
-            backgroundImage: `
-            linear-gradient(${gridColor} 1px, transparent 1px),
-            linear-gradient(90deg, ${gridColor} 1px, transparent 1px)
-          `,
-            backgroundSize: '40px 40px',
-            backgroundPosition: '0 0, 0 0',
-          }
-        }}
-      >
-        <Container
-          maxWidth='xl'
-          sx={{
-            display: 'flex',
-            flexDirection: "column",
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}
-        >
-          {/* TODO: надо добавить лого */}
-          <Typography variant="h2" sx={{ padding: 2 }}>
-            Добро пожаловать в &lt;Цитаты Дня&gt;!
-          </Typography>
-          <Typography variant="h4" sx={{ padding: 2, paddingBottom: 8 }}>
-            Ваши любимые мемные цитаты из фильмов и сериалов.
-          </Typography>
-          <Button component={Link} to="/login" sx={{
-            borderRadius: "12px",
-            px: 2.5,
-            boxShadow: 3,
-            color: theme.palette.text.primary,
-            background: theme.palette.primary.main
-          }}>
-            Войти, чтобы начать!
-          </Button>
-        </Container>
-      </Box>
+      <GridBackGroundLayout sx={{ minWidth: '100vw' }}>
+        <Typography variant="h2" sx={{ padding: 2 }}>
+          Добро пожаловать в &lt;Цитаты Дня&gt;!
+        </Typography>
+        <Typography variant="h4" sx={{ padding: 2, paddingBottom: 8 }}>
+          Ваши любимые мемные цитаты из фильмов и сериалов.
+        </Typography>
+        <Button component={Link} to="/login" sx={{
+          borderRadius: "12px",
+          px: 2.5,
+          boxShadow: 3,
+          color: theme.palette.text.primary,
+          background: theme.palette.primary.main
+        }}>
+          Войти, чтобы начать!
+        </Button>
+      </GridBackGroundLayout>
 
-      <Box
-        sx={{
-          minHeight: '100vh',
-          minWidth: '100vw',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}
-      >
-        <Container
-          maxWidth='xl'
-          sx={{
-            display: 'flex',
-            flexDirection: "column",
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}
-        >
-          <Typography variant="h2" sx={{ padding: 2 }}>
-            Какие-то скрины
-          </Typography>
-        </Container>
-      </Box>
+      <CenterFullScreenLayout sx={{ minWidth: '100vw' }}>
+        <Typography variant="h2" sx={{ padding: 2 }}>
+          Какие-то скрины
+        </Typography>
+      </CenterFullScreenLayout>
 
-      <Box
-        sx={{
-          minHeight: '100vh',
-          minWidth: '100vw',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}
-      >
-        <Container
-          maxWidth='xl'
-          sx={{
-            display: 'flex',
-            flexDirection: "column",
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}
-        >
-          <Typography variant="h2" sx={{ padding: 2 }}>
-            Какие-то скрины
-          </Typography>
-        </Container>
-      </Box>
+      <CenterFullScreenLayout sx={{ minWidth: '100vw' }}>
+        <Typography variant="h2" sx={{ padding: 2 }}>
+          Какие-то скрины
+        </Typography>
+      </CenterFullScreenLayout>
     </>
   );
 }
