bfc542eae34e2fd1ccebd70d278a199e362b429d
aragami3070 <smirnov17612@gmail.com>
2026-03-29 21:11:53 +0400

    Feat(login-page): end ui

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,149 +1,70 @@
-import { Box, Button, Container, TextField, Typography } from "@mui/material";
-import { alpha, useTheme } from "@mui/material/styles";
-import { useForm } from "react-hook-form";
 import type { SubmitHandler } from "react-hook-form";
 import { useSelector } from "react-redux";
 import { useEffect } from "react";
 import { login } from "../store/user";
 import { useAppDispatch, type RootState } from "../store";
+import type { AuthFieldConfig } from "../components/AuthTemplatePage";
+import AuthTemplatePage from "../components/AuthTemplatePage";
 
-type LoginFormValues = {
-  email: string;
-  password: string;
-};
-
+// TODO: после успешного Login делать getMe, которая стучится на профиль и выдает User
 export default function Login() {
-  const {
-    register: loginForm,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<LoginFormValues>();
   const dispatch = useAppDispatch();
   const isAuth = useSelector((state: RootState) => state.user.isAuth);
   const isUserLoading = useSelector((state: RootState) => state.user.isUserLoading);
 
   useEffect(() => {
     if (isAuth && !isUserLoading) {
-      console.log("User logged in")
+      console.log("User logged in, but not loading");
+      console.log("Loading user");
     }
   }, [isAuth])
 
-
   const onSubmit: SubmitHandler<LoginFormValues> = async (data: LoginFormValues) => {
-    console.log(data);
     await dispatch(login({
       email: data.email,
       password: data.password
     }))
   };
 
-  const theme = useTheme();
-  const backgroundColor = theme.palette.background.default;
-  const fontColor = theme.palette.text.primary;
   return (
-    <Box
-      sx={(theme) => {
-        const gridColor = alpha(theme.palette.divider, 0.19);
-        return {
-          minHeight: "100vh",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          backgroundImage: `
-            linear-gradient(${gridColor} 1px, transparent 1px),
-            linear-gradient(90deg, ${gridColor} 1px, transparent 1px)
-          `,
-          backgroundSize: "40px 40px",
-        };
-      }}
-    >
-      <Container maxWidth="xs"
-        sx={{
-          display: 'flex',
-          flexDirection: "column",
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}
-      >
-        <Typography variant="h4" sx={{ padding: 1 }}>
-          Вход
-        </Typography>
-        <form onSubmit={handleSubmit(onSubmit)}
-          style={{
-            display: 'flex',
-            flexDirection: "column",
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}
-        >
-
-          <TextField
-            sx={{
-              background: backgroundColor,
-              "& .MuiInputBase-input": {
-                color: fontColor,
-              },
-              "& .MuiInputLabel-root": {
-                color: fontColor,
-              },
-              "& .MuiInputLabel-root.Mui-focused": {
-                color: fontColor,
-              },
-            }}
-            fullWidth
-            label="Email"
-            type="email"
-            margin="normal"
-            {...loginForm("email", {
-              required: "Введите email",
-              pattern: {
-                value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
-                message: "Некорректный email",
-              },
-            })}
-            error={!!errors.email}
-            helperText={errors.email?.message}
-          />
-
-          <TextField
-            sx={{
-              background: backgroundColor,
-              "& .MuiInputBase-input": {
-                color: fontColor,
-              },
-              "& .MuiInputLabel-root": {
-                color: fontColor,
-              },
-              "& .MuiInputLabel-root.Mui-focused": {
-                color: fontColor,
-              },
-            }}
-            fullWidth
-            label="Пароль"
-            type="password"
-            margin="normal"
-            {...loginForm("password", {
-              required: "Введите пароль",
-              minLength: {
-                value: 16,
-                message: "Минимум 16 символов",
-              },
-            })}
-            error={!!errors.password}
-            helperText={errors.password?.message}
-          />
-
-          <Button
-            type="submit"
-            variant="contained"
-            sx={{
-              mt: 2,
-            }}
-          >
-            Создать аккаунт
-          </Button>
-        </form>
-      </Container>
-    </Box>
+    <AuthTemplatePage
+      title="Вход"
+      fields={fields}
+      onSubmit={onSubmit}
+      buttonText="Войти в аккаунт"
+    />
   );
 }
+
+type LoginFormValues = {
+  email: string;
+  password: string;
+};
+
+
+const fields: AuthFieldConfig<LoginFormValues>[] = [
+  {
+    name: "email",
+    label: "Почта",
+    type: "email",
+    rules: {
+      required: "Введите почту",
+      pattern: {
+        value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+        message: "Некорректный почта",
+      },
+    },
+  },
+  {
+    name: "password",
+    label: "Пароль",
+    type: "password",
+    rules: {
+      required: "Введите пароль",
+      minLength: {
+        value: 16,
+        message: "Минимум 16 символов",
+      },
+    }
+  }
+]
