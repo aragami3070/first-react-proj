6e456bd0441b9b9ed43dbe1005b98c4401fe4c6d
aragami3070 <smirnov17612@gmail.com>
2026-04-15 10:16:00 +0400

    fix(guest-wrapper): change children on Outlet

--- src/components/wrappers/GuestWrapper.tsx
+++ src/components/wrappers/GuestWrapper.tsx
@@ -1,14 +1,13 @@
-import type { ReactNode } from "react";
 import { useSelector } from "react-redux";
-import { Navigate } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 import type { RootState } from "../../store";
 
-export const GuestWrapper = ({ children }: { children: ReactNode }) => {
+export const GuestWrapper = () => {
   const isAuth = useSelector((state: RootState) => state.user.isAuth);
 
   if (isAuth) {
     return <Navigate to="/profile" replace />;
   }
 
-  return children;
+  return <Outlet />;
 };
