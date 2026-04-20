acd56363535ae5737be9b766e31647678d4f1bb2
aragami3070 <smirnov17612@gmail.com>
2026-04-20 22:59:06 +0400

    chore(utils): remove useless

--- src/utils/refreshLogic.ts
+++
@@ -1,7 +0,0 @@
-import { store } from "../store";
-import { refresh } from "../store/user";
-
-export async function refreshTokens() {
-  await store.dispatch(refresh())
-  return sessionStorage.getItem("accessToken");
-}
